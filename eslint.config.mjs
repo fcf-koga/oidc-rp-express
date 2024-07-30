6ab8e815c34fd2e6bca1729de6b8435d0916f6fc
fcf-koga <k-koga@future-creation-f.co.jp>
2024-07-31 08:49:49 +0900

    eslintにセミコロンを強制するルールを追加

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -9,6 +9,7 @@ export default [
     },
     rules: {
       "no-var": "error", // varを禁止するルールを追加
+      semi: ["error", "always"], // セミコロンを強制するルールを追加
     },
   },
   { languageOptions: { globals: { ...globals.browser, ...globals.node } } },
